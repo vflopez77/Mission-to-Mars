b8f8bdb8770eaa8bce00091714691f814c2a7aad
vflopez77 <68453606+vflopez77@users.noreply.github.com>
2020-09-24 18:52:41 -0400

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -13,69 +13,80 @@ import pandas as pd
 executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
 browser = Browser('chrome', **executable_path)
 
-# Visit the mars nasa news site
-url = 'https://mars.nasa.gov/news/'
-browser.visit(url)
+def mars_news(browser):
 
-# Optional delay for loading the page
-browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
+    # Visit the mars nasa news site
+    url = 'https://mars.nasa.gov/news/'
+    browser.visit(url)
 
-# Set up HTML parser
-html = browser.html
-news_soup = soup(html, 'html.parser')
+    # Optional delay for loading the page
+    browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
 
-slide_elem = news_soup.select_one('ul.item_list li.slide')
+    # Set up HTML parser
+    html = browser.html
+    news_soup = soup(html, 'html.parser')
 
-slide_elem.find("div", class_='content_title')
+    # Add try/except for error handling
+    try:
+        slide_elem = news_soup.select_one('ul.item_list li.slide')
+        slide_elem.find("div", class_='content_title')
+        # Use the parent element to find the first `div` 'content_title' tag and save it as `news_title`
+        news_title = slide_elem.find("div", class_='content_title').get_text()
+        # Use the parent element to find the first `div` 'article_teaser_body' tang and save it as `news_p` for paragraph
+        news_p = slide_elem.find('div', class_="article_teaser_body").get_text()
 
-# Use the parent element to find the first `div` 'content_title' tag and save it as `news_title`
-news_title = slide_elem.find("div", class_='content_title').get_text()
-news_title
+    except AttributeError:
+        return None, None
 
-# Use the parent element to find the first `div` 'article_teaser_body' tang and save it as `news_p` for paragraph
-news_p = slide_elem.find('div', class_="article_teaser_body").get_text()
-news_p
+    return news_title, news_p
 
 # ## JPL Space Images Featured Image
+def featured_image(browser)
+    # Visit URL
+    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser.visit(url)
 
-# Visit URL
-url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
-browser.visit(url)
+    # Find and click the full image button
+    full_image_elem = browser.find_by_id('full_image')
+    full_image_elem.click()
 
-# Find and click the full image button
-full_image_elem = browser.find_by_id('full_image')
-full_image_elem.click()
+    # Find the more info button and click that
+    browser.is_element_present_by_text('more info', wait_time=1)
+    more_info_elem = browser.links.find_by_partial_text('more info')
+    more_info_elem.click()
 
-# Find the more info button and click that
-browser.is_element_present_by_text('more info', wait_time=1)
-more_info_elem = browser.links.find_by_partial_text('more info')
-more_info_elem.click()
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
 
-# Parse the resulting html with soup
-html = browser.html
-img_soup = soup(html, 'html.parser')
+    # Add try/except for error handling
+    try:
+        # Find the relative image url
+        img_url_rel = img_soup.select_one('figure.lede a img').get("src")
 
-# Find the relative image url
-img_url_rel = img_soup.select_one('figure.lede a img').get("src")
-img_url_rel
+    except AttributeError:
+        return none
 
-# Use the base URL to create an absolute URL
-img_url = f'https://www.jpl.nasa.gov{img_url_rel}'
-img_url
+    # Use the base URL to create an absolute URL
+    img_url = f'https://www.jpl.nasa.gov{img_url_rel}'
+    
+    return img_url
 
 # ## Mars Facts
+def mars_facts():
+    try:
+        # Get HTML facts table as DataFrame
+        df = pd.read_html('http://space-facts.com/mars/')[0]
+        
+    except BaseException:
+        return None
 
-# Get HTML facts table as DataFrame
-df = pd.read_html('http://space-facts.com/mars/')[0]
-df.head()
+    # Name DataFrame columns
+    df.columns=['description', 'value']
+    df.set_index('description', inplace=True)
 
-# Name DataFrame columns
-df.columns=['description', 'value']
-df.set_index('description', inplace=True)
-df
-
-# Turn back into HTML
-df.to_html()
+    # Turn back into HTML
+    return df.to_html()
 
 # Turn off automated browser
 browser.quit()
